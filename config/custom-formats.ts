a2860bf07b1cae67c50e4fed67293c6d85a8cef9
megatyler30 <37842542+megatyler30@users.noreply.github.com>
2021-01-14 12:07:57 -0600

    Create custom-formats.ts

---
+++ config/custom-formats.ts
@@ -0,0 +1,18 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+export const Formats: FormatList = [
+name: "[Gen 8] LCUU",
+		threads: [
+			`&bullet; <a href="https://www.smogon.com/forums/threads/swsh-lcuu-metagame-discussion-thread.3660866/">LC Metagame Discussion</a>`
+		],
+
+		mod: 'gen8',
+		maxLevel: 5,
+		ruleset: ['Little Cup', 'Standard', 'Dynamax Clause'],
+		banlist: [
+			'Corsola-Galar', 'Cutiefly', 'Drifloon', 'Gastly', 'Gothita', 'Rufflet', 'Scyther', 'Sneasel', 'Swirlix', 'Tangela', 'Vulpix-Alola',
+			'Chlorophyll', 'Moody', 'Baton Pass', 'Abra', 'Archen', 'Chinchou', 'Dewpider', 'Diglett', 'Drilbur', 'Dwebble', 'Ferroseed', 'Foongus', 'Grookey', 'Koffing', 
+      'Magnemite', 'Mareanie', 'Mienfoo', 'Mudbray', 'Onix', 'Pawniard', 'Ponyta', 'Ponyta-Galar', 'Porygon', 'Scorbunny', 'Scraggy', 'Spritzee', 'Staryu', 'Timburr',
+      'Trapinch', 'Tyrunt', 'Vullaby', 'Vulpix', 'Dewpider', 'Farfetch'd-Galar, 'Shellder', 'Wingull',
+		],
+];
